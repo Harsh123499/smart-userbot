ded756d80565ed99cbfb4368f2bf8d162101937e
MR HΛRSH <157881731+Harsh123499@users.noreply.github.com>
2024-02-28 14:06:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "22704480")) #optional
+API_ID = int(getenv("API_ID", "29395700")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://te.legra.ph/file/db842c00aec6ebd9503b9.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://te.legra.ph/file/2902084e5fa8c997d867b.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://t.me/TEAM_DST")
+REPO_URL = getenv("REPO_URL", "https://t.me/Sigma_byz")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
